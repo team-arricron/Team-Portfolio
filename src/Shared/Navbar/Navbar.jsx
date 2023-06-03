c442b15a550276afb7ced210c97ec4a50e649f8f
rajutalukdar1 <lalutaluker@gmail.com>
2023-06-03 20:03:34 +0600

    route complte

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,75 @@
-import React from 'react';
+import { Menu } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { BiEditAlt } from "react-icons/bi";
+
 
 const Navbar = () => {
+
+    const menuItem = <>
+        <li>
+            <div className="text-right">
+                <Menu as="div" className="relative inline-block text-left">
+                    <div>
+                        <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-black">
+                            Options
+                            <ChevronDownIcon
+                                className="ml-2 -mr-1 h-5 w-5 "
+                                aria-hidden="true"
+                            />
+                        </Menu.Button>
+                    </div>
+                    <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y rounded-md bg-white shadow-lg ring-1 ring-opacity-5 focus:outline-none">
+                        <div className="px-1 py-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    >
+                                        {active ? (
+                                            <BiEditAlt
+                                                className="mr-2 h-5 w-5"
+                                                aria-hidden="true"
+                                            />
+                                        ) : (
+                                            <BiEditAlt
+                                                className="mr-2 h-5 w-5"
+                                                aria-hidden="true"
+                                            />
+                                        )}
+                                        Archive
+                                    </button>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    >
+                                        {active ? (
+                                            <BiEditAlt
+                                                className="mr-2 h-5 w-5"
+                                                aria-hidden="true"
+                                            />
+                                        ) : (
+                                            <BiEditAlt
+                                                className="mr-2 h-5 w-5"
+                                                aria-hidden="true"
+                                            />
+                                        )}
+                                        Move
+                                    </button>
+                                )}
+                            </Menu.Item>
+                        </div>
+                    </Menu.Items>
+                </Menu>
+            </div>
+        </li>
+
+    </>
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -9,32 +78,14 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {menuItem}
                     </ul>
                 </div>
                 <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {menuItem}
                 </ul>
             </div>
             <div className="navbar-end">
