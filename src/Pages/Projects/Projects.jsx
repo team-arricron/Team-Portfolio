7e118b21a3c1a70a5fe087f1713effe12ada8362
rajutalukdar1 <lalutaluker@gmail.com>
2023-07-10 18:28:17 +0600

    setup

---
+++ src/Pages/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+const Projects = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ src/Pages/Team/Team.jsx
@@ -0,0 +1,11 @@
+const Team = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Team;
