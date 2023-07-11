6eb03e5b081eb217cdf0e6138c6a5cfc08c1e83c
ahadhasanjion <niallhoran3176@gmail.com>
2023-07-12 00:16:14 +0600

    banner

---
+++ src/Banner/Banner.jsx
@@ -0,0 +1,68 @@
+import { Link } from 'react-router-dom';
+import './Banner.css'
+import image1 from '../assets/1.jpg'
+import image2 from '../assets/2.jpg'
+import image3 from '../assets/3.jpg'
+import image4 from '../assets/4.jpg'
+
+const Banner = () => {
+  return (
+    <div className="justify-center mx-auto flex items-center">
+      <div className="links-text flex justify-center text-white">
+        <ul className="grid col-1 relative lg:grid-cols-3 md:gird-cols-2  z-50 text-white">
+          <li  className="project  cursor-pointer p-10">
+            <h2 className=' font-bold text-3xl'>
+              <span className="z-50 font-semibold text-sm mr-4">01.</span>
+              <Link to="" className='inline-block z-50 '>
+                <span className="block font-normal sub-title">Design</span>{" "}
+                <span className="text">Everybody sunshine</span>
+              </Link>
+            </h2>
+            <div className='project-img -z-50'>
+                <img className='img' src={image1} alt=""/>
+            </div>
+          </li>
+          <li  className="project  cursor-pointer p-10">
+            <h2 className=' font-bold text-3xl'>
+              <span className="z-50 font-semibold text-sm mr-4">01.</span>
+              <Link to="" className='inline-block z-50 '>
+                <span className="block font-normal sub-title">Design</span>{" "}
+                <span className="text">Everybody sunshine</span>
+              </Link>
+            </h2>
+            <div className='project-img -z-50'>
+                <img className='img' src={image2} alt=""/>
+            </div>
+          </li>
+          
+          <li  className="project cursor-pointer  p-10">
+            <h2 className=' font-bold text-3xl'>
+              <span className="z-50 font-semibold text-sm mr-4">01.</span>
+              <Link to="" className='inline-block z-50 '>
+                <span className="block font-normal sub-title">Design</span>{" "}
+                <span className="text">Everybody sunshine</span>
+              </Link>
+            </h2>
+            <div className='project-img -z-50'>
+                <img className='img' src={image3} alt=""/>
+            </div>
+          </li>
+          <li  className="project cursor-pointer  p-10">
+            <h2 className=' font-bold text-3xl'>
+              <span className="z-50 font-semibold text-sm mr-4">01.</span>
+              <Link to="" className='inline-block z-50 '>
+                <span className="block font-normal sub-title">Design</span>{" "}
+                <span className="text">Everybody sunshine</span>
+              </Link>
+            </h2>
+            <div className='project-img -z-50'>
+                <img className='img' src={image4} alt=""/>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             This is home
+            <Banner/>
         </div>
     );
 };
